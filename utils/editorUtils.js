ce2892b85ef680f608c152f80fb7216e8c2b07d8
unknown <behrooz.arabzade@gmail.com>
2021-06-23 17:08:04 +0430

    debugging memory leak

--- utils/editorUtils.js
+++ utils/editorUtils.js
@@ -392,7 +392,7 @@ EditorUtils.publishProject = async (path, folder, targetUrl, publisherWebsite, u
 
         let response = await fetch(targetUrl, {
             method: 'post',
-            body: form
+            body: data
         });
 
         source.destroy();
